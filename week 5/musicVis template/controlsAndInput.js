de0605c22dc92e5753958b39fdffc11aa697e334
olawale micheal juwon <olawalejuwon@gmail.com>
2021-05-15 14:33:25 +0100

    completed music visualiser

--- week 5/musicVis template/controlsAndInput.js
+++ week 5/musicVis template/controlsAndInput.js
@@ -1,57 +1,62 @@
 //Constructor function to handle the onscreen menu, keyboard and mouse
 //controls
-function ControlsAndInput(){
-	
-	this.menuDisplayed = false;
-	
-	//playback button displayed in the top left of the screen
-	this.playbackButton = new PlaybackButton();
+function ControlsAndInput() {
+  this.menuDisplayed = false;
 
-	//make the window fullscreen or revert to windowed
-	this.mousePressed = function(){
-		//???
-		//check if the playback button has been clicked
-		//if not make the visualisation fullscreen
-	};
+  //playback button displayed in the top left of the screen
+  this.playbackButton = new PlaybackButton();
 
-	//responds to keyboard presses
-	//@param keycode the ascii code of the keypressed
-	this.keyPressed = function(keycode){
-		console.log(keycode);
-		if(keycode == 32){
-			this.menuDisplayed = !this.menuDisplayed;
-		}
+  //make the window fullscreen or revert to windowed
+  this.mousePressed = function () {
+    //???
+    if (!this.playbackButton.hitCheck()) {
+      let fs = fullscreen();
 
-		if(keycode > 48 && keycode < 58){
-			var visNumber = keycode - 49;
-			vis.selectVisual(vis.visuals[visNumber].name); 
-		}
-	};
+      fullscreen(!fs);
+    }
 
-	//draws the playback button and potentially the menu
-	this.draw = function(){
-		push();
-		fill("white");
-		stroke("black");
-		strokeWeight(2);
-		textSize(34);
+    //check if the playback button has been clicked
+    //if not make the visualisation fullscreen
+  };
 
-		//playback button 
-		this.playbackButton.draw();
-		//only draw the menu if menu displayed is set to true.
-		if(this.menuDisplayed){
+  //responds to keyboard presses
+  //@param keycode the ascii code of the keypressed
+  this.keyPressed = function (keycode) {
+    console.log(keycode);
+    if (keycode == 32) {
+      this.menuDisplayed = !this.menuDisplayed;
+    }
 
-			text("Select a visualisation:", 100, 30);
-			this.menu();
-		}	
-		pop();
+    if (keycode > 48 && keycode < 58) {
+      var visNumber = keycode - 49;
+      vis.selectVisual(vis.visuals[visNumber].name);
+    }
+  };
 
-	};
+  //draws the playback button and potentially the menu
+  this.draw = function () {
+    push();
+    fill("white");
+    stroke("black");
+    strokeWeight(2);
+    textSize(34);
 
-	this.menu = function(){
-		//draw out menu items for each visualisation
-		//???
-	};
+    //playback button
+    this.playbackButton.draw();
+    //only draw the menu if menu displayed is set to true.
+    if (this.menuDisplayed) {
+      text("Select a visualisation:", 100, 30);
+      this.menu();
+    }
+    pop();
+  };
+
+  this.menu = function () {
+    //draw out menu items for each visualisation
+    //???
+ 
+    for (var i = 0; i < vis.visuals.length; i++) {
+      text(i+1+": " + vis.visuals[i].name, 100, 60+i*40)
+    }
+  };
 }
-
-
--- week 5/musicVis template/spectrum.js
+++ week 5/musicVis template/spectrum.js
@@ -1,18 +1,31 @@
-function Spectrum(){
-	this.name = "spectrum";
+function Spectrum() {
+  this.name = "spectrum";
 
-	this.draw = function(){
-		push();
-		var spectrum = fourier.analyze();
-		noStroke();
-		
-		fill(0,255,0)
-		for (var i = 0; i< spectrum.length; i++){
-			var x = map(i, 0, spectrum.length, 0, width);
-		    var h = -height + map(spectrum[i], 0, 255, height, 0);
-		    rect(x, height, width / spectrum.length, h );
-  		}
-	
-		pop();
-	};
+  this.draw = function () {
+    push();
+    var spectrum = fourier.analyze();
+    noStroke();
+
+    fill(0, 255, 0);
+    for (var i = 0; i < spectrum.length; i++) {
+      var cs = spectrum[i];
+	  var lowCol = map(cs, 0, 255, 255, 0)
+      if (cs < 127) {
+        fill(cs, lowCol, 0);
+      } else if (cs === 127) {
+        fill(cs, 127, 0);
+      } else if (cs > 127) {
+        fill(cs, 0, 0);
+      }
+      var x = map(i, 0, spectrum.length, 0, width);
+      // var h = -height + map(spectrum[i], 0, 255, height, 0);
+      var h = -height + map(spectrum[i], 0, 255, height, 0);
+      // rect(x, height, width / spectrum.length, h );
+      // console.log(width/spectrum.length, h)
+      var w = map(spectrum[i], 0, 255, 0, width);
+      rect(0, i, w, 1);
+    }
+
+    pop();
+  };
 }
