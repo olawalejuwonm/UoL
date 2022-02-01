d5a0f975add80bb1fe23b26220e8d8afbc4f90e1
olawalejuwonm <olawalejuwon@gmail.com>
2022-02-01 19:48:07 +0100

    linked list javascript done

---
+++ linked/linked.js
@@ -0,0 +1,63 @@
+// var head = null;
+// head = { data: 5, next: null };
+// console.log(head);
+// console.log(head.data);
+// console.log(head.next);
+
+function LLNode(data) {
+  this.data = data;
+  this.next = null;
+}
+var head = new LLNode(5);
+// console.log(head);
+// console.log(head.data);
+// console.log(head.next);
+head.next = new LLNode(10);
+// console.log(head.next.data);
+// console.log(head.next.next);
+
+//Excercise 1
+var head = new LLNode(4);
+head.next = new LLNode(6);
+head.next.next = new LLNode(8);
+head.next.next.next = new LLNode(10);
+head.next.next.next.next = new LLNode(10);
+
+// console.log(head);
+
+//Loop linked list
+// var temp = head ;
+// while ( temp !== null ) {
+// temp = temp . next ;
+// }
+
+function searchLL(head, item) {
+  var temp = head;
+  while (temp !== null) {
+    if (temp.data === item) {
+      return true;
+    }
+    temp = temp.next;
+  }
+  return false;
+}
+
+// console.log(searchLL(head, 7))
+// console.log(searchLL(head, 10))
+
+function numberLL(head, item) {
+  var temp = head;
+  var seen = 0;
+  while (temp !== null) {
+    if (temp.data === item) {
+      seen++;
+    }
+    temp = temp.next;
+  }
+  return seen;
+}
+
+
+// console.log(numberLL(head, 0))
+// console.log(numberLL(head, 4))
+// console.log(numberLL(head, 10))
