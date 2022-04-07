0942ec1268dc1bee31ac9d5e1128939394bcb64c
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-07 10:30:41 +0100

    week 1

---
+++ week 1/emptyExample/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(720, 400);
+}
+
+
+function draw() {
+    background(220);
+    fill(0);
+    // translate(60, 60)
+    // rect(10, 10, 100, 100);
+
+    // ellipse(0,0,20,20);
+    // ellipse(-60, -60, 20, 20);
+
+    rect(200,100,100,100);
+
+
+    fill(125)
+    rotate(radians(45));
+
+
+}
---
+++ week 1/translate.js
@@ -0,0 +1,10 @@
+function setUp() {
+    createCanvas(720, 400);
+}
+
+
+function draw() {
+    background(220);
+    fill(0);
+    rect(10, 10, 100, 100);
+}
