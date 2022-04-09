7c03226c831e550e4bbac8b460e4835627852cef
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-09 20:30:14 +0100

    before assignment

--- week 1/emptyExample copy/sketch.js
+++ week 1/emptyExample copy/sketch.js
@@ -1,45 +1,54 @@
+const secLength = 160;
+const secWidth = 1;
+const minLength = 140;
+const minWidth = 3;
+const hourLength = 90;
+const hourWidth = 5;
+
 function setup() {
-    createCanvas(720, 400);
-    rectMode(CENTER); // center the rectangle
+  createCanvas(720, 400);
 }
 
-
 function draw() {
-    background(220);
-    fill(0);
-    // translate(60, 60)
-    // rect(10, 10, 100, 100);
+  background(255);
+  translate(width / 2, height / 2);
+  ellipse(0, 0, 350, 350);
 
-    // ellipse(0,0,20,20);
-    // ellipse(-60, -60, 20, 20);
+  push();
+  strokeWeight(secWidth);
+  stroke(200, 0, 0);
+  const secAngle = map(second(), 0, 60, 0, 360); // map the seconds to an angle
+  rotate(radians(secAngle)); // rotate the angle
+  line(0, 0, 0, -secLength); // draw the line
+  pop();
 
+  push();
+  strokeWeight(minWidth);
+  stroke(0);
+  const minAngle = map(minute(), 0, 60, 0, 360); // map the seconds to an angle
+  rotate(radians(minAngle)); // rotate the angle
+  line(0, 0, 0, -minLength); // draw the line
+  pop();
 
-    // rect(200,100,100,100);
+  push();
+  strokeWeight(hourWidth);
+  stroke(0);
+  const hourAngle = map(hour(), 0, 12, 0, 360); // map the seconds to an angle
+  rotate(radians(hourAngle)); // rotate the angle
+  line(0, 0, 0, -hourLength); // draw the line
+  push();
+  translate(0, -hourLength);
+  ellipse(0, 0, 20, 20);
+  pop();
+  pop();
 
-
-    // fill(125)
-    // // rotate(radians(45));
-    // translate(200,100);
-    // scale(0.5, 0.5);
-    // rect(0,0,100,100);
-
-
-    translate(200,100);
-    // rotate(radians(45));
-    rect(0,0, 200, 200);
-
-
+  const hours = 12;
+  const hourStep = 360 / hours;
+  for (let i = 0; i < hours; i++) {
     push();
-    fill(255, 0, 0);
-    translate(100,100);
-    ellipse(0,0,30, 30);
+    rotate(radians(hourStep * i));
+    translate(0, -155);
+    line(0, 0, 0, -20);
     pop();
-
-    push();
-    fill(255, 0, 0);
-    translate(-100,-100);
-    ellipse(0,0,30, 30);
-    pop();
-
-
+  }
 }
