9aefd0b1713c3088b0f2b120480235f748c696cf
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-10 20:48:55 +0100

    assignment done

--- week 1/solarSystem-TEMPLATE/sketch.js
+++ week 1/solarSystem-TEMPLATE/sketch.js
@@ -8,43 +8,47 @@ function draw() {
   background(0);
   speed = frameCount;
 
-  push();
-  translate(width / 2, height / 2);
 
-  celestialObj(color(255, 150, 0), 200); // SUN
+  push();
+    translate(width / 2, height / 2);
+
+    //Step 5
+    rotate(radians(speed/3))
+
+    celestialObj(color(255, 150, 0), 200); // SUN
 
   pop();
 
   push();
-  translate(width / 2, height / 2);
-  //Step 2
-  rotate(radians(speed));
+    translate(width / 2, height / 2);
+    //Step 2
+    rotate(radians(speed));
 
-  translate(300, 0);
-  
-  //Step 3
-  rotate(radians(speed));
+    translate(300, 0);
+    
+    //Step 3
+    rotate(radians(speed));
 
-  //Step 1
-  celestialObj(color(0, 0, 255), 80); // EARTH
+    //Step 1
+    celestialObj(color(0, 0, 255), 80); // EARTH
 
-  //Step 4
-  rotate(radians(speed));
-  translate(100, 0);
-  rotate(radians(speed * -2));
-  celestialObj(color(255, 255, 255), 30); // MOON
+    //Step 4
+    rotate(radians(speed));
+    translate(100, 0);
+    rotate(radians(-speed *2));
+
+    celestialObj(color(255, 255, 255), 30); // MOON
+
+
+    rotate(radians(speed));
+    translate(-50, 0);
+    rotate(radians(-speed*3));
+    celestialObj(color(255, 0, 0), 20); // ASTEROID
+
+
   pop();
 
-  //Step 4
-//   push();
-//   translate(width / 2, height / 2);
-//   rotate(radians(speed));
-//   translate(300, 0);
-//   rotate(radians(speed));
-//   translate(100, 0);
-//   rotate(radians(speed * -2));
-//   celestialObj(color(255, 255, 255), 30); // MOON
-//   pop();
+
 }
 
 function celestialObj(c, size) {
