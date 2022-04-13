66b97c37312759c08c90b489ca5800a1a95b0953
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-13 09:23:45 +0100

    done with using vectors

--- week 2/emptyExample/sketch.js
+++ week 2/emptyExample/sketch.js
@@ -1,8 +1,41 @@
+let ball;
 function setup() {
     createCanvas(900, 600);
     background(0);
+    ball = new Ball()
 }
 
 function draw() {
+    background(0);
+    ball.run()
 
+}
+
+class Ball {
+    constructor() {
+        this.velocity = new createVector(random(-5, 5), random(-5, 5));
+        this.location = new createVector(random(width), random(height));
+    }
+    run() {
+        this.draw();
+        this.move();
+        this.bounce();
+    }
+    draw () {
+        fill(125);
+        ellipse(this.location.x, this.location.y, 40, 40);
+    }
+    bounce (){
+        if (this.location.x > width || this.location.x < 0) {
+            this.velocity.x *= -1;
+        }
+        if (this.location.y > height || this.location.y < 0) {
+            this.velocity.y *= -1;
+        }
+    }
+    
+
+    move() {
+        this.location.add(this.velocity);
+    }
 }
