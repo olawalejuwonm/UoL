833386fd4aef2a1a7f4e649b172f39f04971c92d
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-13 12:07:15 +0100

    week 2 done

--- week 2/followingTheMouse/sketch.js
+++ week 2/followingTheMouse/sketch.js
@@ -4,39 +4,54 @@
 // Adapted from https://github.com/nature-of-code/
 // released under MIT license
 
-var ball;
+var balls = [];
 ///////////////////////////////////////////////
 function setup() {
-  createCanvas(900,600);
-  ball = new Ball();
+  createCanvas(900, 600);
+  background(0);
+  for (var i = 0; i < 100; i++) {
+    balls.push(new Ball());
+  }
 }
 ////////////////////////////////////////////////
 function draw() {
-  background(0);
-  ball.run();
+  balls.forEach(function(ball) {
+    ball.run();
+  }
+  );
+  
 }
 ///////////////////////////////////////////////
 class Ball {
-
-  constructor(){
+  constructor() {
     this.velocity = new createVector(0, 0);
-    this.location = new createVector(width/2, height/2);
     this.acceleration = new createVector(0, 0);
-    this.maxVelocity = 5;
+    this.maxVelocity = 7;
+    var randomX = width / 2 + random(-100, 100);
+    var randomY = height / 2 + random(-100, 100);
+    this.prevLocation = new createVector(randomX, randomY);
+    this.location = new createVector(randomX, randomY);
+
   }
 
-  run(){
+  run() {
     this.draw();
     this.move();
-    this.edges();
+    // this.edges();
   }
 
-  draw(){
-    fill(125);
-    ellipse(this.location.x, this.location.y, 40, 40);
+  draw() {
+    stroke(color(random(255), random(255), random(255)));
+    strokeWeight(random(1, 3));
+    
+    line(this.location.x, this.location.y, this.prevLocation.x, this.prevLocation.y);
+    this.prevLocation = this.location.copy();
+    // fill(125);
+
+    // ellipse(this.location.x, this.location.y, 40, 40);
   }
 
-  move(){
+  move() {
     var mouse = createVector(mouseX, mouseY);
     var dir = p5.Vector.sub(mouse, this.location);
     dir.normalize();
@@ -47,10 +62,10 @@ class Ball {
     this.location.add(this.velocity);
   }
 
-  edges(){
-    if (this.location.x<0) this.location.x=width;
-    else if (this.location.x>width) this.location.x = 0;
-    else if (this.location.y<0) this.location.y = height;
-    else if (this.location.y>height) this.location.y = 0;
+  edges() {
+    if (this.location.x < 0) this.location.x = width;
+    else if (this.location.x > width) this.location.x = 0;
+    else if (this.location.y < 0) this.location.y = height;
+    else if (this.location.y > height) this.location.y = 0;
   }
 }
