2459557c6c52806a9c38dbecd30cb49f4a496666
olawalejuwonm <olawalejuwon@gmail.com>
2022-04-27 12:53:12 +0100

    fixes

--- topic2/myFirstExpress/index.js
+++ topic2/myFirstExpress/index.js
@@ -3,4 +3,6 @@ const express = require("express");
 const app = express();
 const port = 8083;
 app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>")); 
+app.get("/search", (req, res) => res.send("Welcome to search page") )
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
