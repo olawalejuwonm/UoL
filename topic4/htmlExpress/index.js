e2a1173a4a3a2d247ce845f4453d620fd687129c
olawalejuwonm <olawalejuwon@gmail.com>
2022-05-03 15:26:36 +0100

    week 8 done

--- topic4/htmlExpress/index.js
+++ topic4/htmlExpress/index.js
@@ -1,6 +1,10 @@
 // The index.js file of your application
 const express = require ("express");
+const bodyParser= require ("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 8086;
 require("./routes/main")(app); 
 app.set("views",__dirname + "/views"); 
--- topic4/htmlExpress/routes/main.js
+++ topic4/htmlExpress/routes/main.js
@@ -1,12 +1,40 @@
 // The main.js file of your application
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-      res.render("index.html");
-    });
-    app.get("/search", function (req, res) {
-      res.render("search.html");
-    });
-    app.get("/about", function (req, res) {
-      res.render("about.html");
-    });
-  };    
+  app.get("/", function (req, res) {
+    res.render("index.html");
+  });
+  app.get("/search", function (req, res) {
+    res.render("search.html");
+  });
+  app.get("/about", function (req, res) {
+    res.render("about.html");
+  });
+
+  app.get("/search-result", function (req, res) {
+    //searching in the database
+    res.send(req.query);
+    // res.send(
+    //   "This is the keyword you entered: " +
+    //     req.query.keyword +
+    //     "<br>" +
+    //     "This is the result of the search:"
+    // );
+  });
+
+  app.get("/register", function (req, res) {
+    res.render("register.html");
+  });
+  app.post("/registered", function (req, res) {
+    // saving data in database
+    // res.send(req.body)
+    res.send(
+      "Hello " +
+        req.body.first +
+        " " +
+        req.body.last +
+        ", you are now registered!" +
+        "We'll send email to:" +
+        req.body.email
+    );
+  });
+};
