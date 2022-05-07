58ee8c956703c30698cfd9625d3bac1a0bf84e52
olawalejuwonm <olawalejuwon@gmail.com>
2022-05-07 18:31:08 +0100

    try

--- week 5/angryBirdClone-TEMPLATE/physics.js
+++ week 5/angryBirdClone-TEMPLATE/physics.js
@@ -1,73 +1,107 @@
 ////////////////////////////////////////////////////////////////
-function setupGround(){
+function setupGround() {
   ground = Bodies.rectangle(500, 600, 1000, 40, {
-    isStatic: true, angle: 0
+    isStatic: true,
+    angle: 0,
   });
   World.add(engine.world, [ground]);
 }
 
 ////////////////////////////////////////////////////////////////
-function drawGround(){
+function drawGround() {
   push();
   fill(128);
   drawVertices(ground.vertices);
   pop();
 }
 ////////////////////////////////////////////////////////////////
-function setupPropeller(){
+function setupPropeller() {
   // your code here
+  propeller = Bodies.rectangle(150, 480, 200, 15, {
+    angle,
+    isStatic: true,
+  });
+  World.add(engine.world, [propeller]);
 }
 ////////////////////////////////////////////////////////////////
 //updates and draws the propeller
-function drawPropeller(){
+function drawPropeller() {
   push();
   // your code here
+  Body.setAngle(propeller, angle);
+  Body.setAngularVelocity(propeller, angleSpeed);
+  angle += angleSpeed;
+  drawVertices(propeller.vertices);
   pop();
 }
 ////////////////////////////////////////////////////////////////
-function setupBird(){
-  var bird = Bodies.circle(mouseX, mouseY, 20, {friction: 0,
-      restitution: 0.95 });
-  Matter.Body.setMass(bird, bird.mass*10);
+function setupBird() {
+  var bird = Bodies.circle(mouseX, mouseY, 20, {
+    friction: 0,
+    restitution: 0.95,
+  });
+  Matter.Body.setMass(bird, bird.mass * 10);
   World.add(engine.world, [bird]);
   birds.push(bird);
 }
 ////////////////////////////////////////////////////////////////
-function drawBirds(){
+function drawBirds() {
   push();
   //your code here
+  for (var i = 0; i < birds.length; i++) {
+    drawVertices(birds[i].vertices);
+
+    if (isOffScreen(birds[i])) {
+      removeFromWorld(birds[i]);
+      birds.splice(i, 1);
+      i--;
+    }
+  }
   pop();
 }
 ////////////////////////////////////////////////////////////////
 //creates a tower of boxes
-function setupTower(){
+function setupTower() {
   //you code here
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 6; j++) {
+      var b = Bodies.rectangle(100 + i * 50, 100 + j * 50, 80, 80)
+      World.add(engine.world, [b]);
+      boxes.push(b);
+      colors.push(color(0, 255, 0, random(0, 255)));
+    }
+  }
 }
 ////////////////////////////////////////////////////////////////
 //draws tower of boxes
-function drawTower(){
+function drawTower() {
   push();
   //your code here
+  translate(700, 100);
+  for (var i = 0; i < boxes.length; i++) {
+    fill(colors[i]);
+    drawVertices(boxes[i].vertices);
+  }
   pop();
 }
 ////////////////////////////////////////////////////////////////
-function setupSlingshot(){
-//your code here
+function setupSlingshot() {
+  //your code here
 }
 ////////////////////////////////////////////////////////////////
 //draws slingshot bird and its constraint
-function drawSlingshot(){
+function drawSlingshot() {
   push();
   // your code here
   pop();
 }
 /////////////////////////////////////////////////////////////////
-function setupMouseInteraction(){
+function setupMouseInteraction() {
   var mouse = Mouse.create(canvas.elt);
   var mouseParams = {
     mouse: mouse,
-    constraint: { stiffness: 0.05 }
-  }
+    constraint: { stiffness: 0.05 },
+  };
   mouseConstraint = MouseConstraint.create(engine, mouseParams);
   mouseConstraint.mouse.pixelRatio = pixelDensity();
   World.add(engine.world, mouseConstraint);
