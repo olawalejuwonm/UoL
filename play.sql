8ec5511daa4456cae87d8e09da390287045177d7
olawalejuwonm <olawalejuwon@gmail.com>
2022-05-07 14:41:21 +0100

    week 10

---
+++ play.sql
@@ -0,0 +1,8 @@
+-- CREATE database student;
+USE student;
+-- CREATE TABLE Food (id INT AUTO_INCREMENT, name VARCHAR(50), price DECIMAL(5,2) unsigned, PRIMARY KEY(id));
+-- INSERT INTO Food VALUES ( 'Pizza', 6.25);
+-- INSERT INTO Food VALUES(NULL, 'Pizza', 6.25),(NULL, 'Chips', 2.00);
+-- INSERT INTO Food (name, price) VALUES ('Pizza', 6.25);
+
+-- INSERT INTO Food VALUES ( 'Pizza', 6.25);
