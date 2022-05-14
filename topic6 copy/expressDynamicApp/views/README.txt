addb0be1492b62c79b7d261af659cca9485f94e7
olawalejuwonm <olawalejuwon@gmail.com>
2022-05-14 15:29:00 +0100

    done with topic 6

--- topic6 copy/expressDynamicApp/views/README.txt
+++
@@ -1,1 +0,0 @@
-The views folder contains all of your html pages.
--- topic6/6.sql
+++ topic6/6.sql
@@ -1,7 +1,10 @@
 use myBookShop;
 select * from books;
+INSERT INTO books (name, price) VALUES ("Musick's Monument", 19.5);
 UPDATE books SET price=18.99 where id=2;
-DELETE from books where id=2;
+DELETE from books where id=123;
+SELECT * FROM books WHERE id=123;  
+DELETE FROM books WHERE name="Express book";
 
 
 Use myRestaurantMenu;
@@ -9,4 +12,4 @@ Show tables;
 select * from dishes;
 UPDATE dishes SET name='cheese and bacon sandwich',price=3.50,
 is_vegetarian=0,is_vegan=0 where id=3;
-DELETE from dishes where id=1;
+DELETE from dishes where id=3;
