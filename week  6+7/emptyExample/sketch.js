20524e8deeb9d43ccc75158f2ebe2d8ad3de82fa
olawalejuwonm <olawalejuwon@gmail.com>
2022-05-19 07:52:42 +0100

    edit

--- week  6+7/emptyExample/sketch.js
+++ week  6+7/emptyExample/sketch.js
@@ -26,13 +26,14 @@ class TickleWalker {
   }
 }
 
-function setup() {
-  createCanvas(900, 600);
-  background(0);
-  tickle = new TickleWalker(width / 2, height / 2);
+// function setup() {
+//   createCanvas(900, 600);
+//   background(0);
+//   tickle = new TickleWalker(width / 2, height / 2);
 
-  rectMode(CENTER);
-}
+//   rectMode(CENTER);
+// }
+
 
 // function draw() {
 //     var randX = random(0, width);
@@ -53,25 +54,110 @@ function setup() {
 // }
 //Noise
 var time = 0;
+// function draw() {
+//   background(0);
+
+//   var nx = noise(time);
+
+//   var locX = map(nx, 0, 1, 0, width);
+//   var g = map(nx, 0, 1, 0, 255);
+
+//   var nY = noise(time + 5);
+//   var locY = map(nY, 0, 1, 0, height);
+  
+//   var rotZ = map(nx, 0, 1, -25, 25);
+
+
+
+//   translate(locX, locY);
+//   rotate(rotZ);
+
+//   fill(0, g, 0);
+//   rect(0, 0, 50, 50);
+//   time += 0.01;
+// }
+
+////2D Noise
+// function setup() {
+//   createCanvas(100, 100);
+//   background(0);
+//   rectMode(CENTER);
+
+// }
+
+// function draw() {
+//   background(0);
+//   // randomGrid();
+//   noiseGrid();
+//   noLoop();
+// }
+
+// function noiseGrid() {
+//   for (var x = 0; x < width; x ++) {
+//     for (var y = 0; y < height; y ++) {
+//       var n = noise(x/10, y/10);
+//       var c = map(n, 0, 1, 0, 255);
+
+//       stroke(c);
+//       point(x, y);
+
+//       // var n = random(0, 1);
+//       // if (n > 0.5) {
+//       //   fill(0, 255, 0);
+//       //   rect(x, y, 50, 50);
+//       // }
+//     }
+//   }
+// }
+
+// function randomGrid() {
+//   for (var x = 0; x < width; x ++) {
+//     for (var y = 0; y < height; y ++) {
+//       var c = random(0, 255);
+//       stroke(c);
+//       point(x, y);
+
+//       // var n = random(0, 1);
+//       // if (n > 0.5) {
+//       //   fill(0, 255, 0);
+//       //   rect(x, y, 50, 50);
+//       // }
+//     }
+//   }
+// }
+
+
+//3D Noise
+
+function setup() {
+  createCanvas(200, 200);
+  background(0);
+  rectMode(CENTER);
+
+}
+
 function draw() {
   background(0);
-
-  var nx = noise(time);
-
-  var locX = map(nx, 0, 1, 0, width);
-  var g = map(nx, 0, 1, 0, 255);
-
-  var nY = noise(time + 5);
-  var locY = map(nY, 0, 1, 0, height);
-  
-  var rotZ = map(nx, 0, 1, -25, 25);
-
-
-
-  translate(locX, locY);
-  rotate(rotZ);
-
-  fill(0, g, 0);
-  rect(0, 0, 50, 50);
-  time += 0.01;
+  // randomGrid();
+  noiseGrid();
+  // noLoop();
 }
+
+function noiseGrid() {
+  for (var x = 0; x < width; x ++) {
+    for (var y = 0; y < height; y ++) {
+      var n = noise(x/100, y/100, frameCount/100);
+      var c = map(n, 0, 1, 0, 255);
+
+      stroke(c);
+      point(x, y);
+
+      // var n = random(0, 1);
+      // if (n > 0.5) {
+      //   fill(0, 255, 0);
+      //   rect(x, y, 50, 50);
+      // }
+    }
+  }
+}
+
