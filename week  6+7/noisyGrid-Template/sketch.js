5a2928b3561f8525e93de922f9c2bfc175819ee9
olawalejuwonm <olawalejuwon@gmail.com>
2022-06-27 15:04:53 +0100

    again

--- week  6+7/noisyGrid-Template/sketch.js
+++ week  6+7/noisyGrid-Template/sketch.js
@@ -33,28 +33,24 @@ function colorGrid(){
 
 }
 ///////////////////////////////////////////////////////////////////////
-function compassGrid() {
+function compassGrid(){
   // your code here
   //TODO: Amend the compassGrid() function to create a grid of 25x25 lines of length stepSize. Make sure each compass is at the center of each tile. By default they should all be pointing up. You should use translate() to move to the center of each grid.
   for (var x = 0; x < width; x += stepSize) {
     for (var y = 0; y < height; y += stepSize) {
-      push();
       translate(x + stepSize / 2, y + stepSize / 2);
-      //TODO: For each of the compasses generate a 3D noise value, using the compass’ x and y coordinate as well as the frameCount so that the noise values change over time. Make sure you scale the input parameters of the noise function appropriately so that you get organic values out of it. Use that noise value with map() to generate an angle between 0 and 720 degrees. Use that value to rotate the compass.
-      //TODO: Make sure you scale the input parameters of the noise function appropriately so that you get organic values out of it
-          
-     
-      const n = noise(
-        x / 10000 + mouseX,
-        y / 10000 + mouseX,
-        frameCount / 10000 + mouseX
-      );
-
-      const angle = map(n, 0, 1, 0, 720);
+      // stroke(0);
+      // line(0, 0, 0, -stepSize);
+      //TODO:  For each of the compasses generate a 3D noise value, using the compass’ x and y coordinate as well as the frameCount so that the noise values change over time. Make sure you scale the input parameters of the noise function appropriately so that you get organic values out of it. Use that noise value with map() to generate an angle between 0 and 720 degrees. Use that value to rotate the compass.
+      let noiseValue = noise((x * noiseScale) + mouseX, y * noiseScale, frameCount * noiseScale);
+      let colorValue = map(mouseX, 0, width, 0, 255);
+      let angle = map(noiseValue, 0, 1, 0, 720);
       rotate(angle);
       stroke(0);
-      line(0, 0, 0, stepSize);
-      pop();
+      line(0, 0, 0, -stepSize);
+      translate(-x - stepSize / 2, -y - stepSize / 2);
+      
+
     }
   }
 }
