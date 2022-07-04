72288eb768fc400f7626d4cb50bab85363fdc8e6
olawalejuwonm <olawalejuwon@gmail.com>
2022-07-04 13:12:28 +0100

    complete all functionality

--- midterm/routes/main.js
+++ midterm/routes/main.js
@@ -60,4 +60,46 @@ module.exports = function (app) {
       res.render("control-device", { devices: result, device: device });
     });
   });
+  app.post("/control-device", function (req, res) {
+    //This post request will be used to update the status of the device using the name of the device
+    let sqlquery = "UPDATE devices SET status = ? WHERE name = ?";
+    let newrecord = [req.body.status, req.body.name];
+    console.log("New record", newrecord);
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        console.log("An error occurred while updating data", err);
+        res.redirect("/");
+      }
+      console.log("Data updated successfully", result);
+      res.redirect("/list-devices");
+    });
+  });
+  app.get("/delete-device", function (req, res) {
+    let sqlquery = "SELECT * FROM devices";
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        console.log("An error occurred while selecting data", err);
+        res.redirect("/");
+      }
+      console.log("Data selected successfully", result);
+      const id = req.query.id;
+      let device = result.find((device) => device.id == id); //This will return the device with the id that was passed in the query
+      console.log("Device found", device);
+      res.render("delete-device", { devices: result, device: device });
+    });
+  });
+  app.post("/delete-device", function (req, res) {
+    //This  uses the device name to delete the device and related data from the database
+    let sqlquery = "DELETE FROM devices WHERE name = ?";
+    let newrecord = [req.body.name];
+    console.log("New record", newrecord);
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        console.log("An error occurred while deleting data", err);
+        res.redirect("/");
+      }
+      console.log("Data deleted successfully", result);
+      res.redirect("/list-devices");
+    });
+  });
 };
