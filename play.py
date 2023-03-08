0a30e9ef31028367f1c2ee08a89bd182de29d1f8
olawalejuwonm <olawalejuwon@gmail.com>
2023-03-08 15:38:45 +0100

    complete topic 9

--- play.py
+++ play.py
@@ -1,1 +1,17 @@
 print(type("3"))
+
+lis = [0.001897551842619749, 0.36665401210408377, 0.500231585811176, 0.2335408085353584, 0.13013973930730338, 0.9541308488663596, 0.5094259633880269, 0.47659169935611745, 0.4892149525599945, 0.5274724260328211]
+#calculate the mean
+mean = sum(lis)/len(lis)
+print(mean)
+
+# Question 3
+# What is the mean of the values in data?
+
+x = [2,4,6,8,10]
+
+data = [v*2 for v in x]
+
+mean = sum(data)/len(data)
+print(mean)
+
