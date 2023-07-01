c1347bd045c1dede6f2e460eef110352a48d553a
olawalejuwonm <olawalejuwon@gmail.com>
2023-07-01 17:00:48 +0100

    add requirement.txt

--- Mid Term/proteins/proteins/urls.py
+++ Mid Term/proteins/proteins/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import api
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # TODO: Homepage that'll display all links
+    path('api/protein/<int:pk>/', api.proteinRequestHandler, name='protein_api'),
 ]
--- topic5/bioweb/genedata/api.py
+++ topic5/bioweb/genedata/api.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import *
@@ -11,33 +11,34 @@ from rest_framework import generics
 from rest_framework import mixins
 
 
-# @api_view(['GET', 'PUT', 'POST', 'DELETE'])
-# def gene_detail(request, pk):
-#
-#     if request.method == 'POST':
-#         serializer = GeneSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     try:
-#         gene = Gene.objects.get(pk=pk)
-#     except Gene.DoesNotExist:
-#         return HttpResponse(status=404)
-#     if request.method == 'GET':
-#         serializer = GeneSerializer(gene)
-#         return Response(serializer.data)
-#     elif request.method == 'DELETE':
-#         gene.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-#
-#     elif request.method == 'PUT':
-#         serializer = GeneSerializer(gene, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT', 'POST', 'DELETE'])
+def gene_detail(request, pk):
+    print(request, pk , "request pk")
+
+    if request.method == 'POST':
+        serializer = GeneSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        gene = Gene.objects.get(pk=pk)
+    except Gene.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':
+        serializer = GeneSerializer(gene)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        gene.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = GeneSerializer(gene, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class GeneDetail(mixins.CreateModelMixin,
                  mixins.RetrieveModelMixin,
