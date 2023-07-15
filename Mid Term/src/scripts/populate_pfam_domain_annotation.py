7711b22f3c2af5d1b4907f394966fb0ab52fd60f
olawalejuwonm <olawalejuwon@gmail.com>
2023-07-15 13:37:46 +0100

    seed all domain anotation except exceptions

--- Mid Term/src/scripts/populate_pfam_domain_annotation.py
+++ Mid Term/src/scripts/populate_pfam_domain_annotation.py
@@ -14,7 +14,7 @@ django_project_dir = os.path.normpath(os.path.join(dirname, '../'))
 
 # ../datas/assignment_data_sequences.csv
 # Get the path of the CSV file by joining the current directory with '../../datas/assignment_data_sequences.csv'
-csv_file = os.path.normpath(os.path.join(dirname, '../../datas/pfam_descriptions.csv'))
+csv_file = os.path.normpath(os.path.join(dirname, '../../datas/assignment_data_set.csv'))
 
 # Print the directory of the Django project and the path of the CSV file
 print("django_project_dir: ", django_project_dir)
@@ -31,6 +31,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cell.settings')
 django.setup()
 
 from pfam.models import DomainAnnotation as PfamDomainAnnotation
+from pfam.models import Domain as PfamDomain
+from protein.models import Detail as ProteinDetail
 
 
 # Delete all records from the DomainAnnotation table
@@ -41,6 +43,7 @@ with open(csv_file, 'r') as f:
     reader = csv.reader(f)
     # Print the csv reader object
     print("reader: ", reader)
+    exception_rows = []
     # Iterate over each row in the csv reader object
     for row in reader:
         # Print the current row
@@ -49,18 +52,27 @@ with open(csv_file, 'r') as f:
         if row[0] == '':
             continue
         # Create a new PfamDomainAnnotation object with the protein_id and sequence fields set to the values in the current row
-        pfam_domain_annotation = PfamDomainAnnotation.objects.create(
-            protein=row[0],
-            pfam=row[5],
-            start=row[6],
-            stop=row[7],
-            taxa_id=row[1],
-            name=row[3],
-            clade=row[2],
-            description=row[4],
-            length=row[8],
-        )
+        try:
+            pfam_domain_annotation = PfamDomainAnnotation.objects.create(
+                protein=ProteinDetail.objects.get(protein_id=row[0]),
+                pfam= PfamDomain.objects.get(domain_id=row[5]),
+                start=row[6],
+                stop=row[7],
+                taxa_id=row[1],
+                name=row[3],
+                clade=row[2],
+                description=row[4],
+                length=row[8],
+            )
+        # catches all exceptions
+        except Exception as e:
+            print("Error: ", e)
+            print("row: ", row)
+            exception_rows.append(row)
+            continue
+        
         # Print the new PfamDomainAnnotation object
         print("pfam_domain_annotation: ", pfam_domain_annotation)
     # Print a success message after all rows have been processed
     print("Successfully populated PfamDomainAnnotation table")
+    print("Exception rows: ", exception_rows)
