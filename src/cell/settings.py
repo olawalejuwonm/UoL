1fce5c692fd567f7c70a9b62270f1948e9bd4e65
olawalejuwonm <olawalejuwon@gmail.com>
2023-07-15 16:23:34 +0100

    add python version

--- src/cell/settings.py
+++ src/cell/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
