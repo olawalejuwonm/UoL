c79acb1ff6133c214950f81969bb3f1d85ffc9bd
Olawale Micheal Juwon <olawalejuwonm@gmail.com>
2023-08-08 14:10:00 +0100

    format

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -15,28 +15,28 @@ MainComponent::MainComponent()
 {
     // Make sure you set the size of the component after
     // you add any child components.
-    setSize (800, 600);
+    setSize(800, 600);
 
     // Some platforms require permissions to open input channels so request that here
-    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
-        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    if (RuntimePermissions::isRequired(RuntimePermissions::recordAudio) && !RuntimePermissions::isGranted(RuntimePermissions::recordAudio))
     {
-        RuntimePermissions::request (RuntimePermissions::recordAudio,
-                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
-    }  
+        RuntimePermissions::request(RuntimePermissions::recordAudio,
+                                    [&](bool granted)
+                                    { if (granted)  setAudioChannels (2, 2); });
+    }
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels (0, 2);
+        setAudioChannels(0, 2);
     }
 
     addAndMakeVisible(playButton);
     addAndMakeVisible(stopButton);
     addAndMakeVisible(loadButton);
-       
+
     addAndMakeVisible(volSlider);
     addAndMakeVisible(speedSlider);
-    
+
     playButton.addListener(this);
     stopButton.addListener(this);
     loadButton.addListener(this);
@@ -44,10 +44,6 @@ MainComponent::MainComponent()
     speedSlider.addListener(this);
 
     volSlider.setRange(0.0, 1.0);
-
-
-
-    
 }
 
 MainComponent::~MainComponent()
@@ -57,33 +53,50 @@ MainComponent::~MainComponent()
 }
 
 //==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     phase = 0.0;
     dphase = 0.0001;
 
     formatManager.registerBasicFormats();
 
-
     transportSource.prepareToPlay(
-        samplesPerBlockExpected, 
+        samplesPerBlockExpected,
         sampleRate);
     resampleSource.prepareToPlay(
-        samplesPerBlockExpected, 
+        samplesPerBlockExpected,
         sampleRate);
-
- }
-void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+}
+void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill)
 {
-    //transportSource.getNextAudioBlock(bufferToFill);
+    // bufferToFill.clearActiveBufferRegion()
+
+    auto *leftChan = bufferToFill.buffer->getWritePointer(0,
+                                                          bufferToFill.startSample);
+    auto *rightChan = bufferToFill.buffer->getWritePointer(0,
+                                                           bufferToFill.startSample);
+    for (auto i = 0; i < bufferToFill.numSamples; ++i)
+    {
+        // double sample = rand.nextDouble() * 0.25;
+        double sample = fmod(phase, 0.2);
+        //  double sample = sin(phase) * 0.1;
+
+        leftChan[i] = sample;
+        rightChan[i] = sample;
+
+        phase += 0.05;
+
+        // phase += dphase;
+    }
+    // transportSource.getNextAudioBlock(bufferToFill);
     resampleSource.getNextAudioBlock(bufferToFill);
 }
 
 // void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
 // {
-//     auto* leftChan = bufferToFill.buffer->getWritePointer(0, 
+//     auto* leftChan = bufferToFill.buffer->getWritePointer(0,
 //                                                          bufferToFill.startSample);
-//     auto* rightChan = bufferToFill.buffer->getWritePointer(0, 
+//     auto* rightChan = bufferToFill.buffer->getWritePointer(0,
 //                                                          bufferToFill.startSample);
 
 //     for (auto i=0; i < bufferToFill.numSamples ; ++i)
@@ -96,8 +109,8 @@ void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFil
 //         rightChan[i] = sample;
 
 //         phase += dphase;
-        
-//     }                                                    
+
+//     }
 //     //bufferToFill.clearActiveBufferRegion();
 // }
 
@@ -111,10 +124,10 @@ void MainComponent::releaseResources()
 }
 
 //==============================================================================
-void MainComponent::paint (Graphics& g)
+void MainComponent::paint(Graphics &g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
     // You can add your drawing code here!
 }
@@ -124,59 +137,56 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    double rowH = getHeight() / 5; 
+    double rowH = getHeight() / 5;
     playButton.setBounds(0, 0, getWidth(), rowH);
     stopButton.setBounds(0, rowH, getWidth(), rowH);
-    
+
     volSlider.setBounds(0, rowH * 2, getWidth(), rowH);
     speedSlider.setBounds(0, rowH * 3, getWidth(), rowH);
 
     loadButton.setBounds(0, rowH * 4, getWidth(), rowH);
-
 }
 
-void MainComponent::buttonClicked(Button* button)
+void MainComponent::buttonClicked(Button *button)
 {
     if (button == &playButton)
     {
         std::cout << "Play button was clicked " << std::endl;
         transportSource.start();
     }
-     if (button == &stopButton)
+    if (button == &stopButton)
     {
         std::cout << "Stop button was clicked " << std::endl;
         transportSource.stop();
-
     }
     if (button == &loadButton)
     {
-        
-        // this does not appear to work in current version of JUCE 
+
+        // this does not appear to work in current version of JUCE
         // (6.1) despite appearing in the docs
         // it might a macro that disables it. TBC
-        //FileChooser chooser{"Select a file..."};
+        // FileChooser chooser{"Select a file..."};
         // if (chooser.browseForFileToOpen())
         // {
         //     loadURL(URL{chooser.getResult()});
         // }
-        // 
+        //
         // this does work in 6.1 but the syntax is a little funky
         // https://docs.juce.com/master/classFileChooser.html#ac888983e4abdd8401ba7d6124ae64ff3
         // - configure the dialogue
-        auto fileChooserFlags = 
-        FileBrowserComponent::canSelectFiles;
+        auto fileChooserFlags =
+            FileBrowserComponent::canSelectFiles;
         // - launch out of the main thread
         // - note how we use a lambda function which you've probably
-        // not seen before. Please do not worry too much about that. 
-        fChooser.launchAsync(fileChooserFlags, [this](const FileChooser& chooser)
-        {
+        // not seen before. Please do not worry too much about that.
+        fChooser.launchAsync(fileChooserFlags, [this](const FileChooser &chooser)
+                             {
             auto chosenFile = chooser.getResult();
-            loadURL(URL{chosenFile});
-        });
+            loadURL(URL{chosenFile}); });
     }
 }
 
-void MainComponent::sliderValueChanged (Slider *slider)
+void MainComponent::sliderValueChanged(Slider *slider)
 {
     if (slider == &volSlider)
     {
@@ -186,22 +196,18 @@ void MainComponent::sliderValueChanged (Slider *slider)
     if (slider == &speedSlider)
     {
         resampleSource.setResamplingRatio(slider->getValue());
-
-        
     }
-
 }
 
 void MainComponent::loadURL(URL audioURL)
 {
 
-    auto* reader = formatManager.createReaderFor(audioURL.createInputStream(false));
+    auto *reader = formatManager.createReaderFor(audioURL.createInputStream(false));
     if (reader != nullptr) // good file!
-    {       
-        std::unique_ptr<AudioFormatReaderSource> newSource (new AudioFormatReaderSource (reader, 
-true)); 
-        transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);             
-        readerSource.reset (newSource.release());          
+    {
+        std::unique_ptr<AudioFormatReaderSource> newSource(new AudioFormatReaderSource(reader,
+                                                                                       true));
+        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+        readerSource.reset(newSource.release());
     }
-
 }
