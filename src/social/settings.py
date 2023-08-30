3c3097a2c3ccb92b5db2a69668f6187006a63c06
olawalejuwonm <olawalejuwon@gmail.com>
2023-08-30 11:33:03 +0100

    clean auth

--- src/social/settings.py
+++ src/social/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # For API
-    'auth' # For auth app
+    # 'auth' # For auth app
 ]
 
 MIDDLEWARE = [
--- src/social/urls.py
+++ src/social/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
-from auth.views import UserViewSet
+# from auth.views import UserViewSet
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+# router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
