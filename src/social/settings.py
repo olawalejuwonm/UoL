44daa3651298f06f4a8e29a6ce6bf7b94fe5128e
olawalejuwonm <olawalejuwon@gmail.com>
2023-08-30 11:24:38 +0100

    update auth

--- src/social/settings.py
+++ src/social/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework' # For API
+    'rest_framework', # For API
+    'auth' # For auth app
 ]
 
 MIDDLEWARE = [
--- src/social/urls.py
+++ src/social/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import routers
+from auth.views import UserViewSet
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
-
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
