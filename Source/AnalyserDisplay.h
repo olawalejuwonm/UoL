554d387d4325f1fbf7ccaaa1028bfa882cdcb35d
olawalejuwonm <olawalejuwon@gmail.com>
2023-09-11 23:36:39 +0100

    comment out display

--- Source/AnalyserDisplay.h
+++ Source/AnalyserDisplay.h
@@ -1,33 +1,33 @@
-/*
-  ==============================================================================
+// /*
+//   ==============================================================================
 
-    AnalyserDisplay.h
-    Created: 11 Sep 2023 7:34:33pm
-    Author:  USER
+//     AnalyserDisplay.h
+//     Created: 11 Sep 2023 7:34:33pm
+//     Author:  USER
 
-  ==============================================================================
-*/
+//   ==============================================================================
+// */
 
-#pragma once
+// #pragma once
 
-#include <JuceHeader.h>
+// #include <JuceHeader.h>
 
-//==============================================================================
-/*
- */
-class AnalyserDisplay : public juce::Component
-{
-public:
-  AnalyserDisplay();
-  ~AnalyserDisplay() override;
+// //==============================================================================
+// /*
+//  */
+// class AnalyserDisplay : public juce::Component
+// {
+// public:
+//   AnalyserDisplay();
+//   ~AnalyserDisplay() override;
 
-  void paint(juce::Graphics &) override;
-  void resized() override;
+//   void paint(juce::Graphics &) override;
+//   void resized() override;
 
-  void drawNextFrameOfSpectrum();
+//   void drawNextFrameOfSpectrum();
 
-  void drawFrame(juce::Graphics &g);
+//   void drawFrame(juce::Graphics &g);
 
-private:
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AnalyserDisplay)
-};
+// private:
+//   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AnalyserDisplay)
+// };
