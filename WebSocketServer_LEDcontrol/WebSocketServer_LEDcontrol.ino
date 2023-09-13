07c89478265eb9d229ef50d31d700bb60e7b6a73
olawalejuwonm <olawalejuwon@gmail.com>
2023-09-13 19:45:41 +0100

    complete websocket

--- WebSocketServer_LEDcontrol/WebSocketServer_LEDcontrol.ino
+++ WebSocketServer_LEDcontrol/WebSocketServer_LEDcontrol.ino
@@ -14,62 +14,66 @@
 #include <ESP8266mDNS.h>
 #include <Hash.h>
 
-#define LED_RED     15
-#define LED_GREEN   12
-#define LED_BLUE    13
+#define LED_RED D2
+#define LED_GREEN D3
+#define LED_BLUE D4
 
 #define USE_SERIAL Serial
-
 
 ESP8266WiFiMulti WiFiMulti;
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
 
-    switch(type) {
-        case WStype_DISCONNECTED:
-            USE_SERIAL.printf("[%u] Disconnected!\n", num);
-            break;
-        case WStype_CONNECTED: {
-            IPAddress ip = webSocket.remoteIP(num);
-            USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+    switch (type)
+    {
+    case WStype_DISCONNECTED:
+        USE_SERIAL.printf("[%u] Disconnected!\n", num);
+        break;
+    case WStype_CONNECTED:
+    {
+        IPAddress ip = webSocket.remoteIP(num);
+        USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
 
-            // send message to client
-            webSocket.sendTXT(num, "Connected");
-        }
-            break;
-        case WStype_TEXT:
-            USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
-
-            if(payload[0] == '#') {
-                // we get RGB data
-
-                // decode rgb data
-                uint32_t rgb = (uint32_t) strtol((const char *) &payload[1], NULL, 16);
-
-                analogWrite(LED_RED,    ((rgb >> 16) & 0xFF));
-                analogWrite(LED_GREEN,  ((rgb >> 8) & 0xFF));
-                analogWrite(LED_BLUE,   ((rgb >> 0) & 0xFF));
-            }
-
-            break;
+        // send message to client
+        webSocket.sendTXT(num, "Connected");
     }
+    break;
+    case WStype_TEXT:
+        USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
 
+        if (payload[0] == '#')
+        {
+            // we get RGB data
+
+            // decode rgb data
+            uint32_t rgb = (uint32_t)strtol((const char *)&payload[1], NULL, 16);
+
+            analogWrite(LED_RED, ((rgb >> 16) & 0xFF));
+            analogWrite(LED_GREEN, ((rgb >> 8) & 0xFF));
+            analogWrite(LED_BLUE, ((rgb >> 0) & 0xFF));
+        }
+
+        break;
+    }
 }
 
-void setup() {
-    //USE_SERIAL.begin(921600);
+void setup()
+{
+    // USE_SERIAL.begin(921600);
     USE_SERIAL.begin(115200);
 
-    //USE_SERIAL.setDebugOutput(true);
+    // USE_SERIAL.setDebugOutput(true);
 
     USE_SERIAL.println();
     USE_SERIAL.println();
     USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
+    for (uint8_t t = 4; t > 0; t--)
+    {
         USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
@@ -83,9 +87,11 @@ void setup() {
     digitalWrite(LED_GREEN, 1);
     digitalWrite(LED_BLUE, 1);
 
+    // TODO: Add server credentials
     WiFiMulti.addAP("SSID", "passpasspass");
 
-    while(WiFiMulti.run() != WL_CONNECTED) {
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
         delay(100);
     }
 
@@ -93,15 +99,16 @@ void setup() {
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
 
-    if(MDNS.begin("esp8266")) {
+    if (MDNS.begin("esp8266"))
+    {
         USE_SERIAL.println("MDNS responder started");
     }
 
     // handle index
-    server.on("/", []() {
+    server.on("/", []()
+              {
         // send index.html
-        server.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>");
-    });
+        server.send(200, "text/html", "<html><head><script>var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);connection.onopen = function () {  connection.send('Connect ' + new Date()); }; connection.onerror = function (error) {    console.log('WebSocket Error ', error);};connection.onmessage = function (e) {  console.log('Server: ', e.data);};function sendRGB() {  var r = parseInt(document.getElementById('r').value).toString(16);  var g = parseInt(document.getElementById('g').value).toString(16);  var b = parseInt(document.getElementById('b').value).toString(16);  if(r.length < 2) { r = '0' + r; }   if(g.length < 2) { g = '0' + g; }   if(b.length < 2) { b = '0' + b; }   var rgb = '#'+r+g+b;    console.log('RGB: ' + rgb); connection.send(rgb); }</script></head><body>LED Control:<br/><br/>R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/></body></html>"); });
 
     server.begin();
 
@@ -112,10 +119,10 @@ void setup() {
     digitalWrite(LED_RED, 0);
     digitalWrite(LED_GREEN, 0);
     digitalWrite(LED_BLUE, 0);
-
 }
 
-void loop() {
+void loop()
+{
     webSocket.loop();
     server.handleClient();
 }
