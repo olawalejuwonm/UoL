4c813e843979ecb273d9848644a4ae40ab51c253
olawalejuwonm <olawalejuwon@gmail.com>
2023-09-18 00:58:21 +0100

    add tests.py

--- src/authn/tests.py
+++ src/authn/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from authn.models import User
 
-# Create your tests here.
+class AuthenticationTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.com',
+            password='testpass'
+        )
+        self.token = Token.objects.create(user=self.user)
+
+    def test_authentication_required(self):
+        url = reverse('status-update-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_token_authentication(self):
+        url = reverse('status-update-list')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_username_password_authentication(self):
+        url = reverse('auth-login')
+        data = {
+            'username': 'testuser',
+            'password': 'testpass'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('token', response.data)
+
+    def test_logout(self):
+        url = reverse('auth-logout')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(Token.objects.filter(user=self.user).exists())
