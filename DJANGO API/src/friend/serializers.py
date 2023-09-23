e938913a1f629fc5502a0fd21164249b1825d16d
olawalejuwonm <olawalejuwon@gmail.com>
2023-09-23 23:23:48 +0100

    remove django and react

--- DJANGO API/src/friend/serializers.py
+++
@@ -1,9 +0,0 @@
-# friend/serializers.py
-from rest_framework import serializers
-from .models import Friend
-
-class FriendSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Friend
-        fields = ['id', 'user', 'friend', 'created_at', 'confirmed']
-        read_only_fields = ['id', 'created_at', 'user', 'confirmed']
